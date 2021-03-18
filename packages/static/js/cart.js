e4927a42f3e139913f6dea7ca8c33de6c53ad854
DFilippov <porkcork@yandex.ru>
2021-03-19 00:09:11 +0300

    Refactor cart.js - improve addToCart function logic, remove garbage code, add comments

--- packages/static/js/cart.js
+++ packages/static/js/cart.js
@@ -1,48 +1,75 @@
 function addToCart(item_id) {
-    let key = `item_id_${item_id}`;
-    storage = window.localStorage
-    var item_quantity = storage.getItem(key);
-    item_quantity = item_quantity * 1 + 1;
-    storage.setItem(key, item_quantity);
+    let cartExists = localStorage.getItem('cart') != null;
+    if (cartExists == false) {
+        localStorage.setItem('cart', null);
+    }
 
+    updateCart(item_id);
     updateNumberInCartLabel(item_id);
     updateCartButton();
 }
 
-function itemsInCartCount(item_id) {
-    let key = `item_id_${item_id}`;
-    let item_quantity = window.localStorage.getItem(key);
+// updates data in 'cart'-object of localStorage (in the cart the key is item's id)
+function updateCart(item_id) {
+    let cart = localStorage.getItem('cart');
+    let cartObject = JSON.parse(cart);
+    if (cartObject == null) {
+        cartObject = {}
+    }
+    let key = item_id;
+    if (cartObject[key] != null) {
+        let newValue = cartObject[key] * 1 + 1
+        cartObject[key] = newValue
+    } else {
+        cartObject[key] = 1
+    }
+
+    updated = JSON.stringify(cartObject)
+    localStorage.setItem('cart', updated)
+}
+
+// will get number of the particular item in the cart (in 'cart'-object of localStorage)
+// (in the cart the key is item's id)
+function numberOfItemInCart(item_id) {
+    let key = item_id;
+    let cart = window.localStorage.getItem('cart');
+    let cartObject = JSON.parse(cart);
+    let item_quantity = cartObject[key];
     return item_quantity;
 }
+
 
 // updates label of the item with number of already added items to cart
 function updateNumberInCartLabel(item_id) {
     id = `number_in_cart_${item_id}`
     label = document.getElementById(id);
-    label.innerHTML = 'Already in cart - ' + itemsInCartCount(item_id);
+    label.innerHTML = 'Already in cart - ' + numberOfItemInCart(item_id);
     label.type = '';
 }
 
-function hideNumberInCartLabel(item_id) {
-    label = document.getElementById('number_in_cart_')
-    label.type = 'hidden'
-}
-
-function getNumberOfItemsInCart() {
-    storage = window.localStorage;
-    number_of_items_in_cart = 0;
-    for (var i = 0; i < storage.length; i++) {
-        let key = storage.key(i);
-        if (key.startsWith('item_id_')) {;
-            value = storage.getItem(key)
-            number_of_items_in_cart += Number.parseInt(value);
-        }
-    }
-    return number_of_items_in_cart;
+function getNumberOfAllItemsInCart() {
+    let cart = localStorage.getItem('cart');
+    let cartObject = JSON.parse(cart);
+    values = Object.values(cartObject);
+    total_value = values.reduce( function(sum, value) {
+            return sum + value
+        }, 0
+    );
+    return total_value;
 }
 
 function updateCartButton() {
-    button = document.getElementById('cart_button')
-    console.log('getNumberOfItemsInCart', getNumberOfItemsInCart())
-    button.innerHTML = `( ${getNumberOfItemsInCart()} )`
+    button = document.getElementById('cart_button_label');
+    button.innerHTML = `( ${getNumberOfAllItemsInCart()} )`;
+}
+
+function getDataFromLocalStorage() {
+    let data = localStorage.getItem('cart');
+    return data;
+}
+
+function updateCartDataElement() {
+    data = getDataFromLocalStorage();
+    cartDataElement = document.getElementById('cart_data');
+    cartDataElement.value = data
 }
